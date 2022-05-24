0201056dbfb821fa656472200de737efcaf78ad7
Adedoyin Adedipe <mywebcreativities@gmail.com>
2022-05-24 14:55:39 +0200

    Using regular expressions in route.

--- index.js
+++ index.js
@@ -5,10 +5,29 @@ const app = express();
 const port = process.env.PORT || 8081; //if first is available, it will use it else it will use 8080.
 
 //adding a static middleware.
-app.use(express.static(__dirname+'/public')); 
+// app.use(express.static(__dirname+'/public')); 
 
 app.get('/', (request, response)=>{
     response.send('<h1>Hello Test</h1>')
+})
+
+//pattern matches (not regular expression - just regular patterns)
+app.get('/ab?cd', (req, res)=>{
+    res.send('<h2>ABC</h2>');
+})
+app.get('/ab+de', (req, res)=>{
+    res.send('<h2>AB+C</h2>');
+})
+app.get('/ab+de', (req, res)=>{
+    res.send('<h2>AD+DE</h2>');
+})
+app.get('/pq*rs', (req, res)=>{//match any string of any length between q and r.
+    res.send('<h2>PQ*RS</h2>');
+})
+
+//regular expressions
+app.get(/z/, (request, response)=>{
+    response.send('<h2>URL file must have a \'z\' in it.');
 })
 
 app.listen(port, ()=>{
